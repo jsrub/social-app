36fc6b5e81ef75ce38c9455c1cd34f0ee32c540e
abhishek <abspotify12@gmail.com>
2019-05-30 03:14:08 +0530

    new

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+//Connect To DB
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Running');
 });
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(4000, (req, res) => {
   console.log(`Server Port ${PORT}`);
